162fa6dcdd060ca4dc3c6c7128d14562f656e11b
helenemartin <helenemartin13@hotmail.com>
2017-07-18 18:36:35 +0100

    add conditional statement for what to wear today

--- script.js
+++ script.js
@@ -29,8 +29,11 @@ function retrieveWeather(){
 
         if (dataForToday.precipProbability  !== undefined && dataForToday.precipProbability  !== null) {
             $("#precipProbability").text("Chance of Rain :" + dataForToday.precipProbability ) 
-            if (dataForToday.precipProbability > 0.2) {
-                
+            if (dataForToday.precipProbability < 0.2) {
+                $("#clothing").attr("src", "/img/swimsuit.jpg")
+            }
+            else{
+                $("#clothing").attr("src", "/img/burber.jpg")
             }
 
         }
