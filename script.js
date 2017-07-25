1b6ca937fe6c3ddf339b6cf3fba2aff2eb3f39f2
helenemartin <helenemartin13@hotmail.com>
2017-07-25 21:00:45 +0100

    fix time without the extra 0s, pull location in html view

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ function retrieveWeather(){
 
     }).done(function(data){
 
+        console.log(data);
+
         var dataForToday = data.daily.data[0];
 
-        var t = dataForToday.time;
-        
+        console.log(dataForToday);
+        console.log(dataForToday.time);
+        // var t = dataForToday.time;
+        // var date = new Date(t*1000);
 
-        var date = new Date(t*1000);
+                // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(dataForToday.time*1000);
 
-        console.log("using data for:" , date)
+        var formatedDate = (date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear());
+        console.log(date);
+        // Hours part from the timestam
+       
+
+        console.log("using data for:" , formatedDate)
 
 
 
-
-        $("#todaysDate").text("Today's date :" + date)
+         $("#location").text("You are in : " + data.timezone)
+        $("#todaysDate").text("Today's date : " + formatedDate)
 
         if (dataForToday.temperatureMax !== undefined && dataForToday.temperatureMax !== null) {
             $("#temperatureMax").text("Max temp :" + dataForToday.temperatureMax)
@@ -27,10 +38,10 @@ function retrieveWeather(){
         if (dataForToday.precipProbability  !== undefined && dataForToday.precipProbability  !== null) {
             $("#precipProbability").text("Chance of Rain :" + dataForToday.precipProbability ) 
             if (dataForToday.precipProbability < 0.2) {
-                $("#clothing").attr("src", "/img/swimsuit.jpg")
+                $("#clothing").attr("src", "img/swimsuit.jpg")
             }
             else{
-                $("#clothing").attr("src", "/img/burber.jpg")
+                $("#clothing").attr("src", "img/burber.jpg")
             }
 
         }
