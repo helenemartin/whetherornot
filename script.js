e044d51d9e57a35688ad63add72812bc51c7b150
helenemartin <helenemartin13@hotmail.com>
2017-10-11 20:24:20 +0100

    create a function for clothing recommandation

--- script.js
+++ script.js
@@ -2,20 +2,63 @@ var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturda
 
 var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-var advice = {
-    "0.10": "very likely",
-    "0.64": "likely", 
-    "0.80": "unlikely",
-    "1": "none",
+
+function clothingRecommandation(data){
+    var advice = {
+        "0.10": "very likely",
+        "0.64": "likely", 
+        "0.80": "unlikely",
+        "1": "none",
+    }
+    var dataForToday = data.daily.data[0];
+    if (dataForToday.precipProbability  !== undefined && dataForToday.precipProbability  !== null) {
+        var message = advice;
+        var advicestring = parseFloat([dataForToday.precipProbability]);
+        switch(true) {
+            case advicestring <= 0.1:
+                return {text:"Very likely", image: "img/burber.jpg"}
+                break;
+            case advicestring < 0.65:
+                return {text:"Likely", image: "img/burber.jpg"}
+                break;
+            case advicestring < 0.81:
+                return {text:"Unlikely", image: "img/swimsuit.jpg"}
+                break;
+            case advicestring <= 1:
+                return {text:"none", image: "img/swimsuit.jpg"}
+                break;
+            default:
+        }
+    }
 }
 
 function extractCity(data){
-
     var zone = data.timezone;
     var city = zone.split("/");
-    return city;
+    return city[city.length-1];
 }
 
+function extractDate(data){
+    var dataForToday = data.daily.data[0];
+    var date = new Date(dataForToday.time*1000);
+    var dayweek = date.getDay();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    return  days[dayweek] + " " + day + " " + months[month] + " " + year;
+    
+}
+
+
+function formatTemperature(data){
+    var dataForToday = data.daily.data[0];
+    if (dataForToday.temperatureMax !== undefined && dataForToday.temperatureMax !== null) {
+        var tempcelsius = Math.round((dataForToday.temperatureMax-32)/1.8);
+        return tempcelsius;
+    }else{
+        return "Temperature not available";
+    }
+}
 
 function retrieveWeather(){
     $.ajax({
@@ -25,86 +68,22 @@ function retrieveWeather(){
 
     }).done(function(data){
 
-        console.log(data);
-
-        var dataForToday = data.daily.data[0];
-
-        console.log(dataForToday);
-        console.log(dataForToday.time);
-        // var t = dataForToday.time;
-        // var date = new Date(t*1000);
-
-                // Create a new JavaScript Date object based on the timestamp
-        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-        var date = new Date(dataForToday.time*1000);
-        var dayweek = date.getDay();
-        var day = date.getDate();
-        var month = date.getMonth();
-        var year = date.getFullYear();
-        var formatedDate = days[dayweek] + " " + day + " " + months[month] + " " + year;
-        //var formatedDate = (date.toDateString());
-        console.log(date);
-        // Hours part from the timestam
-       
-
-        console.log("using data for:" , formatedDate)
+    
         var city = extractCity(data);
+        var formatedDate = extractDate(data);
+        var tempcelsius = formatTemperature(data);
+        var x = clothingRecommandation(data);
         
 
-
-        $("#location").text("You are in " + city[1])
+        $("#location").text("You are in " + city)
         $("#todaysDate").text("Today's date: " + formatedDate)
+        $("#temperatureMax").text("Max temperature: " + tempcelsius + " degrees");
+        $("#precipProbability").text("Chance of rain: " + x.text );
+        $("#clothing").attr("src", x.image);
 
-        if (dataForToday.temperatureMax !== undefined && dataForToday.temperatureMax !== null) {
-
-            var tempcelsius = Math.round((dataForToday.temperatureMax-32)/1.8);
-
-            $("#temperatureMax").text("Max temperature: " + tempcelsius + " degrees");
-        }
-
-        if (dataForToday.precipProbability  !== undefined && dataForToday.precipProbability  !== null) {
-           
-            var message = advice;
-            // console.log('advice' + "helene");
-            var advicestring = parseFloat([dataForToday.precipProbability]);
-            console.log(advicestring);
-            
-
-            switch(true) {
-
-                case advicestring <= 0.1:
-                    $("#precipProbability").text("Chance of rain: " + "very likely" );
-                    $("#clothing").attr("src", "img/burber.jpg");
-                    
-                    break;
-                case advicestring < 0.65:
-                    $("#precipProbability").text("Chance of rain: " + "likely" );
-                    $("#clothing").attr("src", "img/burber.jpg");
-                    
-                    break;
-                case advicestring < 0.81:
-                    $("#precipProbability").text("Chance of rain: " + "unlikely" );
-                    $("#clothing").attr("src", "img/swimsuit.jpg");
-                    break;
-                case advicestring <= 1:
-                    $("#precipProbability").text("Chance of rain: " + "none" );
-                    $("#clothing").attr("src", "img/swimsuit.jpg");
-                    break;
-
-
-
-                default:
-                    console.log("no values");
-
-
-            }
-
-
-        }
-    
+        
 
     });
-
 
 }
 
